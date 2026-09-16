a31a9b86b48bc0acdc99bf50ac231b00d32fa163
Satnam Singh <singhkhowal@gmail.com>
2026-09-16 08:55:27 +0530

    Add shared blog footer partial

---
+++ blogs/_shared/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="ga-site-footer">
+    <div class="ga-container ga-footer-inner">
+        <p>&copy; <?= date('Y') ?> Groot Academy. All rights reserved.</p>
+        <a href="/blogs/">Explore more blogs</a>
+    </div>
+</footer>
+</body>
+</html>
