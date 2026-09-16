e5b624b50b844600c60a4458292f3cd6c354fcbd
Satnam Singh <singhkhowal@gmail.com>
2026-09-16 09:10:35 +0530

    Improve shared blog SEO metadata and schema

--- blogs/_shared/partials/head.php
+++ blogs/_shared/partials/head.php
@@ -6,6 +6,9 @@ $metaDescription = $blog['meta_description'] ?? $blog['excerpt'] ?? '';
 $canonical = $blog['canonical'] ?? '';
 $robots = $blog['robots'] ?? 'index,follow';
 $featuredImage = $blog['featured_image'] ?? '';
+$authorName = $blog['author'] ?? 'Groot Academy';
+$datePublished = $blog['date_published'] ?? '';
+$dateModified = $blog['date_modified'] ?? $datePublished;
 
 $articleSchema = [
     '@context' => 'https://schema.org',
@@ -14,29 +17,64 @@ $articleSchema = [
     'description' => $metaDescription,
     'author' => [
         '@type' => 'Organization',
-        'name' => $blog['author'] ?? 'Groot Academy',
+        'name' => $authorName,
+        'url' => 'https://grootacademy.com/',
     ],
     'publisher' => [
         '@type' => 'Organization',
         'name' => 'Groot Academy',
+        'url' => 'https://grootacademy.com/',
     ],
+    'inLanguage' => 'en-IN',
 ];
 
-if (!empty($blog['date_published'])) {
-    $articleSchema['datePublished'] = $blog['date_published'];
+if ($datePublished) {
+    $articleSchema['datePublished'] = $datePublished;
 }
-if (!empty($blog['date_modified'])) {
-    $articleSchema['dateModified'] = $blog['date_modified'];
+if ($dateModified) {
+    $articleSchema['dateModified'] = $dateModified;
 }
 if ($canonical) {
-    $articleSchema['mainEntityOfPage'] = $canonical;
+    $articleSchema['url'] = $canonical;
+    $articleSchema['mainEntityOfPage'] = [
+        '@type' => 'WebPage',
+        '@id' => $canonical,
+    ];
 }
 if ($featuredImage) {
     $articleSchema['image'] = $featuredImage;
 }
+
+$breadcrumbSchema = null;
+if ($canonical && !empty($blog['title'])) {
+    $breadcrumbSchema = [
+        '@context' => 'https://schema.org',
+        '@type' => 'BreadcrumbList',
+        'itemListElement' => [
+            [
+                '@type' => 'ListItem',
+                'position' => 1,
+                'name' => 'Home',
+                'item' => 'https://grootacademy.com/',
+            ],
+            [
+                '@type' => 'ListItem',
+                'position' => 2,
+                'name' => 'Blogs',
+                'item' => 'https://grootacademy.com/blogs/',
+            ],
+            [
+                '@type' => 'ListItem',
+                'position' => 3,
+                'name' => $blog['title'],
+                'item' => $canonical,
+            ],
+        ],
+    ];
+}
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="en-IN">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -49,10 +87,14 @@ if ($featuredImage) {
     <?php endif; ?>
 
     <meta property="og:type" content="article">
+    <meta property="og:site_name" content="Groot Academy">
+    <meta property="og:locale" content="en_IN">
     <meta property="og:title" content="<?= $esc($metaTitle) ?>">
     <meta property="og:description" content="<?= $esc($metaDescription) ?>">
     <?php if ($canonical): ?><meta property="og:url" content="<?= $esc($canonical) ?>"><?php endif; ?>
     <?php if ($featuredImage): ?><meta property="og:image" content="<?= $esc($featuredImage) ?>"><?php endif; ?>
+    <?php if ($datePublished): ?><meta property="article:published_time" content="<?= $esc($datePublished) ?>"><?php endif; ?>
+    <?php if ($dateModified): ?><meta property="article:modified_time" content="<?= $esc($dateModified) ?>"><?php endif; ?>
 
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="<?= $esc($metaTitle) ?>">
@@ -61,5 +103,8 @@ if ($featuredImage) {
 
     <link rel="stylesheet" href="/blogs/_shared/assets/blog.css">
     <script type="application/ld+json"><?= json_encode($articleSchema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?></script>
+    <?php if ($breadcrumbSchema): ?>
+        <script type="application/ld+json"><?= json_encode($breadcrumbSchema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?></script>
+    <?php endif; ?>
 </head>
 <body>
